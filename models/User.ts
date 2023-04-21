55315a8ef1612c0f3690357dea8311e94889ec4e
Hoang Nguyen <hoangnt219@gmail.com>
2023-04-21 15:11:55 +0700

    User Model

---
+++ models/User.ts
@@ -0,0 +1,44 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import validator from 'validator';
+
+interface User extends Document {
+  name: string;
+  email: string;
+  password: string;
+  role: 'admin' | 'user';
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema: Schema<User> = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide name'],
+      minlength: 3,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'Please provide email'],
+      validate: {
+        validator: (value: string) => validator.isEmail(value),
+        message: 'Please provide valid email',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide password'],
+      minlength: 6,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<User>('User', UserSchema);
