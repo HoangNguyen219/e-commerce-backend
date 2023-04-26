1c61f8c433e5f58c974771429cf8f61cd6a27338
Hoang Nguyen <hoangnt219@gmail.com>
2023-04-26 16:42:54 +0700

    Update error handler

--- middlewares/error-handler.ts
+++ middlewares/error-handler.ts
@@ -2,19 +2,26 @@ import { StatusCodes } from 'http-status-codes';
 import { Request, Response, NextFunction } from 'express';
 
 const errorHandlerMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
-  console.log(err);
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again later',
   };
   if (err.name === 'ValidationError') {
+    const errorArr = Object.values(err.errors)[0] as any;
+    customError.msg = errorArr.message;
     customError.statusCode = StatusCodes.BAD_REQUEST;
   }
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`;
     customError.statusCode = StatusCodes.BAD_REQUEST;
   }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id : ${err.value}`;
+    customError.statusCode = 404;
+  }
+
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
